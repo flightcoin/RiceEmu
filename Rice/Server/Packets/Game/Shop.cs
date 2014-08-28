32fc11e0c9bbc6e28201a7eb20e563b236eab49c
GooMan / GigaToni / ExMex <great.mafia2010%gmail.com@cet.edu>
2014-08-28 14:50:48 +0200

    Thanks to savetherobots for this :)

--- Rice/Server/Packets/Game/Shop.cs
+++ Rice/Server/Packets/Game/Shop.cs
@@ -31,5 +31,30 @@ namespace Rice.Server.Packets.Game
             ack.Writer.Write(quantity);
             packet.Sender.Send(ack);
         }
+        
+        /*
+        public static void DeleteItem(int itemID, int invenID, int count)
+        {
+            Console.WriteLine("Deleting {0}", itemID);
+            Program.GameProxy.SendServer(Packet.Build(403, itemID, invenID, count));
+        }
+
+        public static void SellItem(int itemID, int amount, int invenID)
+        {
+            Console.WriteLine("Selling {0}", itemID);
+            Program.GameProxy.SendServer(Packet.Build(407, itemID, amount, invenID));
+        }
+
+        public static void AuctionItem(int itemID, int amount, int invenID)
+        {
+            Console.WriteLine("Auctioning {0}", itemID);
+            Program.GameProxy.SendServer(Packet.Build(582, itemID, invenID, amount, 9999)); 
+        }
+
+        public static void AuctionCancel(int auctionID)
+        {
+            Console.WriteLine("Cancelling {0}", auctionID);
+            Program.GameProxy.SendServer(Packet.Build(584, auctionID));
+        }*/
     }
 }
