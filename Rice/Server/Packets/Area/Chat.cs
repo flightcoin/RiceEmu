7f26fab735cfbef1ad60b1e6e043f8d83effe567
ExMex <great.mafia2010@gmail.com>
2014-08-28 03:34:02 +0200

    - Added chat - Added shop buy structure

---
+++ Rice/Server/Packets/Area/Chat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rice.Server.Core;
+
+namespace Rice.Server.Packets.Area
+{
+    public static class Chat
+    {
+        [RicePacket(146, RiceServer.ServerType.Game)]
+        public static void ChatWrite(RicePacket packet)
+        {
+            string unknown = packet.Reader.ReadUnicode(); // 114 // room / channel / party / team 
+            string unknown2 = packet.Reader.ReadUnicode(); // 111
+            string unknown3 = packet.Reader.ReadUnicode(); // 111
+
+            unknown = unknown.Substring(0, unknown.Length - 1);
+
+            //Log.WriteLine("Channel: {0} {0} {0}", unknown, unknown2, unknown3);
+
+            //Log.WriteLine("{0}", packet.Reader.ReadUInt32());
+
+            int channel = 0;
+
+            if (unknown.ToLower() == "room")
+                channel = 0;
+            else if (unknown.ToLower() == "channel")
+                channel = 1;
+            else if (unknown.ToLower() == "party")
+                channel = 2;
+            else if (unknown.ToLower() == "team")
+                channel = 3;
+
+            var ack = new RicePacket(147);
+            ack.Writer.WriteUnicode("party  "); // Unknown maybe channel (7 bytes big)
+            ack.Writer.WriteUnicode("GigaToni"); // Username
+            ack.Writer.WriteUnicode("room9999"); // Unknown (8 bytes big)
+            ack.Writer.WriteUnicode("This is the first Chat msg");
+            packet.Sender.Send(ack);
+        }
+    }
+}
---
+++ Rice/Server/Packets/Game/Shop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rice.Server.Core;
+
+namespace Rice.Server.Packets.Game
+{
+    public static class Shop
+    {
+        [RicePacket(405, RiceServer.ServerType.Game)]
+        public static void BuyItem(RicePacket packet)
+        {
+
+            // 0 = ItemID
+            // 1 = Unknown
+            // 2 = Quantity
+            Log.WriteLine("{0} {1} {2}", packet.Reader.ReadInt16(), packet.Reader.ReadInt16(), packet.Reader.ReadInt16());
+        }
+    }
+}
